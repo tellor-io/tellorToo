095e82fdd55ccf7b6deca85324914ea615982ea1
Tim <ultreia0@protonmail.com>
2020-10-08 18:47:16 -0500

    Fill in processChallenge function

--- contracts/CentralizedOracle.sol
+++ contracts/CentralizedOracle.sol
@@ -8,6 +8,8 @@ contract IReceiverStorage {
 
 contract IERC20 {
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
 }
 
 contract CentralizedOracle {
@@ -21,16 +23,18 @@ contract CentralizedOracle {
   uint256 datasetCount;
 
   struct Metadata {
-    uint256 requestId;
-    uint256 timestampWindow;
-    uint256 valueWindow;
-    address token;
-    uint256 minDeposit;
-    uint256 slashAmount;
-    uint256 challengerDeposit;
-    address oracle;
-    uint256 disputeDelay;
-    uint256 oracleBalance;
+    uint256 requestId;              // ID of corresponding mainnet data
+    uint256 timestampWindow;        // Max distance from which a challenge datapoint applies to a centralized datapoint
+    uint256 valueWindow;            // Min distance from which a decentralized value knocks out a centralized value
+    address token;                  // Token used for deposits
+    uint256 minDeposit;             // Minimum balance oracle must have to submit new data
+    uint256 slashAmount;            // How much of deposit lost for inadequate data
+    uint256 challengerDeposit;      // Deposit required to challenge data
+    address oracle;                 // Centralized oracle's address
+    uint256 disputeDelay;           // Delay window for submitting dispute proof
+    uint256 oracleBalance;          // Oracle's token balance
+    uint256 latestTimestamp;        // Last time data was submitted
+    uint256 withdrawalDelay;        // Time delay for withdrawing oracle deposit
   }
 
   struct Challenge {
@@ -57,7 +61,8 @@ contract CentralizedOracle {
       uint256 _slashAmount,
       uint256 _challengerDeposit,
       address _oracle,
-      uint256 _disputeDelay)
+      uint256 _disputeDelay,
+      uint256 _withdrawalDelay)
       public {
 
       metadata[datasetCount] = Metadata({
@@ -70,7 +75,9 @@ contract CentralizedOracle {
           challengerDeposit: _challengerDeposit,
           oracle: _oracle,
           disputeDelay: _disputeDelay,
-          oracleBalance: 0
+          oracleBalance: 0,
+          latestTimestamp: 0,
+          withdrawalDelay: _withdrawalDelay
       });
 
       datasetCount++;
@@ -83,6 +90,7 @@ contract CentralizedOracle {
 
       values[_requestId][_timestamp] = _value;
       set[_requestId][_timestamp] = true;
+      metadata[_requestId].latestTimestamp = now;
   }
 
   function challengeData(uint256 _requestId, uint256 _timestamp) public {
@@ -147,6 +155,30 @@ contract CentralizedOracle {
   }
 
   function processChallenge(uint256 _challengeId) public {
+      Challenge storage challenge = challenges[_challengeId];
+      Metadata storage metaTemp = metadata[challenge.requestId];
+      IERC20 depositToken = IERC20(metaTemp.token);
+      require(now >= challenge.challengeTimestamp + metaTemp.disputeDelay);
+      require(challenge.state != 4);
 
+      if (challenge.state == 2) {
+          metaTemp.oracleBalance -= metaTemp.slashAmount;     // Fix with SafeMath
+          uint256 allowance = depositToken.allowance(address(this), challenge.challenger);
+          depositToken.approve(challenge.challenger, 0);
+          depositToken.approve(challenge.challenger, allowance + metaTemp.slashAmount);
+      } else {
+          metaTemp.oracleBalance += metaTemp.challengerDeposit;
+      }
+
+      challenge.state = 4;
+
+      challenges[_challengeId] = challenge;
+      metadata[challenge.requestId] = metaTemp;
+  }
+
+  function oracleDeposit(uint256 _requestId, uint256 _amount) public {
+      IERC20 depositToken = IERC20(metadata[_requestId].token);
+      require(depositToken.transferFrom(msg.sender, address(this), _amount));
+      metadata[_requestId].oracleBalance += _amount;
   }
 }
