0c56716f317c0485d0436f7efdba52d6d066f160
Tim <ultreia0@protonmail.com>
2020-10-08 21:42:25 -0500

    Parse bytes data from sender to receiver

--- contracts/Receiver.sol
+++ contracts/Receiver.sol
@@ -14,11 +14,25 @@ contract ReceiverStorage {
   function onStateReceive(uint256 stateId, bytes calldata data) external {
     // Check for valid caller
     // Decode bytes data
-    // Save to values datastore
-    // Change 'set' bool
+    (uint256 requestId, uint256 timestamp, uint256 value) = parse96BytesToThreeUint256(data);
+
+    values[requestId][timestamp] = value;   // Save to values datastore
+    set[requestId][timestamp] = true;
   }
 
   function retrieveData(uint256 _requestId, uint256 _timestamp) public view returns(bool, uint256) {
     return(set[_requestId][_timestamp], values[_requestId][_timestamp]);
+  }
+
+  function parse96BytesToThreeUint256(bytes memory data) public returns(uint256, uint256, uint256) {
+    uint256 parsed1;
+    uint256 parsed2;
+    uint256 parsed3;
+    assembly {
+	    parsed1 := mload(add(data, 32))
+	    parsed2 := mload(add(data, 64))
+	    parsed3 := mload(add(data, 96))
+    }
+    return(parsed1, parsed2, parsed3);
   }
 }
