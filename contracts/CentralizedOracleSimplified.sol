8297218d118d7816a51b968fcb7d05be7c7f68fa
Tim <ultreia0@protonmail.com>
2020-10-13 20:33:25 -0500

    Add SafeMath and timestamps & isDisputed mappings for UsingTellor functionality

--- contracts/CentralizedOracleSimplified.sol
+++ contracts/CentralizedOracleSimplified.sol
@@ -2,16 +2,51 @@
 
 pragma solidity 0.5.16;
 
+library SafeMath {
+
+    function max(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a > b ? a : b;
+    }
+
+    function max(int256 a, int256 b) internal pure returns (uint256) {
+        return a > b ? uint256(a) : uint256(b);
+    }
+
+    function min(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a < b ? a : b;
+    }
+
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function sub(int256 a, int256 b) internal pure returns (int256 c) {
+        if (b > 0) {
+            c = a - b;
+            assert(c <= a);
+        } else {
+            c = a - b;
+            assert(c >= a);
+        }
+
+    }
+}
 contract IReceiverStorage {
   function retrieveData(uint256 _requestId, uint256 _timestamp) public view returns(bool, uint256);
 }
 
 contract CentralizedOracle {
+  using SafeMath for uint256;
+
   IReceiverStorage public receiverStorage;
 
   mapping (uint256 => mapping(uint256 => uint256)) public values;
   mapping (uint256 => mapping(uint256 => bool)) public set;
   mapping (uint256 => mapping(uint256 => bool)) public locked;
+  mapping (uint256 => mapping(uint256 => bool)) public isDisputed;
+  mapping(uint256 => uint256[]) public timestamps;
   mapping (uint256 => Metadata) public metadata;
   uint256 datasetCount;
   address owner;
@@ -51,17 +86,19 @@ contract CentralizedOracle {
       require(!locked[_requestId][_timestamp]);
 
       values[_requestId][_timestamp] = _value;
+      timestamps[_requestId].push(_timestamp);
       set[_requestId][_timestamp] = true;
   }
 
   function challengeData(uint256 _requestId, uint256 _timestamp, uint256 _challengeTimestamp) public {
       require(set[_requestId][_timestamp]);
-      require(_challengeTimestamp - _timestamp <= metadata[_requestId].timestampWindow);            // SafeMath
+    //   require(_challengeTimestamp - _timestamp <= metadata[_requestId].timestampWindow);            // SafeMath
+      require(_challengeTimestamp.max(_timestamp).sub(_challengeTimestamp.min(_timestamp)) <= metadata[_requestId].timestampWindow);
 
       (bool retrieved, uint256 retrievedValue) = receiverStorage.retrieveData(metadata[_requestId].referenceRequestId, _challengeTimestamp);
       require(retrieved);
       require(retrievedValue - values[_requestId][_timestamp] > metadata[_requestId].valueWindow);  // SafeMath
-
+      require(retrievedValue.max(values[_requestId][_timestamp]).sub(retrievedValue.min(values[_requestId][_timestamp])) > metadata[_requestId].valueWindow);
       locked[_requestId][_timestamp] = true;
       values[_requestId][_timestamp] = retrievedValue;
   }
