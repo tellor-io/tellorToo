9bddce502151d33775b907a77c5b25ccdc3d850a
brendaloya <l.loya.b@gmail.com>
2020-10-29 22:18:43 -0400

    edit flat file

--- flatfiles/flat_ReceiverStorage.sol
+++ flatfiles/flat_ReceiverStorage.sol
@@ -5,9 +5,9 @@
 pragma solidity 0.5.16;
 
 // IStateReceiver represents interface to receive state
-interface IStateReceiver {
-  function onStateReceive(uint256 stateId, bytes calldata data) external;
-}
+// interface IStateReceiver {
+//   function onStateReceive(uint256 stateId, bytes calldata data) external;
+// }
 
 /**
 @title ReceiverStorage
